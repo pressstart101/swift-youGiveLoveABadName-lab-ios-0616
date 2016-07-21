1d1a1b6eb23d48c30051daa5406bd01ce7cc93b9
pressstart101 <walterscott404@yandex.com>
2016-07-20 22:32:13 -0400

    nice haircut, bon

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,15 +9,104 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var niceHaircut: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    var heightConstraint: NSLayoutConstraint?
+    var flag: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      //  niceHaircut.removeConstraints(niceHaircut.constraints)
+       // niceHaircut.translatesAutoresizingMaskIntoConstraints = false
+        
+        niceHaircut.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
+        niceHaircut.widthAnchor.constraintEqualToAnchor(view.widthAnchor).active = true
+       heightConstraint = niceHaircut.heightAnchor.constraintEqualToAnchor(view.heightAnchor, multiplier: 0.40)
+        heightConstraint?.active = true
+
     }
     
     @IBAction func expandButtonTapped(sender: AnyObject) {
-        
+        print("work!")
+        if self.flag == false{
+        viewUp()
+        }else{viewDown()
+        }
     }
 
+    func viewUp() {
+        UIView.animateKeyframesWithDuration(1.1, delay: 0.0, options: [], animations: {
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.2, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint? = self.niceHaircut.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.2)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+                
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.2, relativeDuration: 0.7, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint? = self.niceHaircut.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1.2)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+                
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.9, relativeDuration: 0.2, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint? = self.niceHaircut.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1.0)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+                
+            })
+            
+            }, completion: {(true) in
+                self.view.bringSubviewToFront(self.button)
+                self.flag = true
+        })
+        
+    }
+    
+    
+    
+    func viewDown(){
+    
+        UIView.animateKeyframesWithDuration(1.3, delay: 0.0, options: [], animations: {
+            
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.2, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint? = self.niceHaircut.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1.2)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+                
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.2, relativeDuration: 0.9, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint? = self.niceHaircut.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.2)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+                
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(1.1, relativeDuration: 0.2, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint? = self.niceHaircut.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.4)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+                
+            })
+            
+            }, completion: {(true) in
+                self.view.bringSubviewToFront(self.button)
+                self.flag = false
+        })
+    }
+    
+    
+    
+    
+    
 }
-
